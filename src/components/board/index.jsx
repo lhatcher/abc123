35a1ef757ffe76e693b14c9445f9cd9ab7664709
Liam Hatcher <hatcherpiano@gmail.com>
2016-10-08 23:41:44 -0500

    Setup and config

---
+++ src/components/board/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from './style.scss';
+
+export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return (
+      <h1>Test</h1>
+    );
+  }
+}
+
+Board.propTypes = {
+
+};
---
+++ src/containers/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Board from '../components/board/index';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Board />
+      </div>
+    );
+  }
+}
